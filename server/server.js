45d9208ee6f7b68f730db2499357326dc6c0fe87
prgh <pghattamaneni@gmail.com>
2017-09-22 22:23:15 -0700

    chat app

--- server/server.js
+++ server/server.js
@@ -14,6 +14,42 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log('new user connected');
+    
+    // socket.emit('newEmail',{
+    //     from: 'abc@example.com',
+    //     text: 'hello',
+    //     createdAt: 123
+    // });
+
+    // socket.on('createEmail', (newEmail)=>{
+    //     console.log('createEmail: ', newEmail);
+    // })
+
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'welcome to chat',
+        createdAt: new Date().getTime
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'new user joined',
+        createdAt: new Date().getTime
+    });
+
+    socket.on('createMessage', (message)=>{
+        console.log('message: ', message);
+        io.emit('newMessage',{
+            from: message.from,
+            test: message.text,
+            createdAt: new Date().getTime
+        });
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     test: message.text,
+        //     createdAt: new Date().getTime
+        // });
+    });
 
     socket.on('disconnect',()=>{
         console.log('user has disconnected');
