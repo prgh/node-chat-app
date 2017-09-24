6c31423a26e32cdf91b47f5394fd413ae0e1dfa3
prgh <pghattamaneni@gmail.com>
2017-09-23 23:42:10 -0700

    add room

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ const http = require('http');
 const express =  require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -27,14 +30,27 @@ io.on('connection',(socket)=>{
     //     console.log('createEmail: ', newEmail);
     // })
 
-    socket.emit('newMessage', generateMessage('Admin','welcome to chat'));
+    
 
-    socket.broadcast.emit('newMessage',generateMessage('Admin','new user joined'));
+    socket.on('join',(params, callback)=>{
+        if (!isRealString(params.name) || !isRealString(params.room)){
+           return callback('name and room name are required');
+        } 
+
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+
+        socket.emit('newMessage', generateMessage('Admin','welcome to chat'));
+        socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
+        callback();
+    });
 
     socket.on('createMessage', (message, callback)=>{
         console.log('message: ', message);
         io.emit('newMessage',generateMessage(message.from,message.text));
-        callback('this is from server');
+        callback();
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
         //     test: message.text,
@@ -42,8 +58,17 @@ io.on('connection',(socket)=>{
         // });
     });
 
+    socket.on('createLocationMessage',(coords)=>{
+        io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude, coords.longtitude));
+    });
+
     socket.on('disconnect',()=>{
         console.log('user has disconnected');
+        var user = users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+            io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left.`));
+        }
     })
 })
 
